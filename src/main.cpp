ec1aba6e6bb7a619ddaf530e307f6bdc13b1147e
schef <zvanstefan@gmail.com>
2020-06-04 18:49:02 +0200

    added watchdog

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "vaga.hpp"
 #include "console.hpp"
 #include "console_test.hpp"
+#include <avr/wdt.h>
 
 static void keepAliveMessage() {
     static uint32_t powerManagerCounter = 0;
@@ -61,17 +62,21 @@ void testTimer() {
     if (firstTime && AppTimer::getInstance().millisPassed(timestamp) >= timeoutFirst) {
         loggif("first time\n");
         firstTime = false;
-        loggif("%lu timestamp, millispassed %lu, timeout %lu\n", (uint32_t)AppTimer::getInstance().getMillis(), (uint32_t)AppTimer::getInstance().millisPassed(timestamp), (uint32_t)timeoutFirst);
+        loggif("%lu timestamp, millispassed %lu, timeout %lu\n", (uint32_t) AppTimer::getInstance().getMillis(), (uint32_t) AppTimer::getInstance().millisPassed(timestamp), (uint32_t) timeoutFirst);
         timestamp = AppTimer::getInstance().getMillis();
         sendSms();
 
     } else if (AppTimer::getInstance().millisPassed(timestamp) >= timeoutNext) {
         loggif("next time\n");
-        loggif("%lu timestamp, millispassed %lu, timeout %lu\n", (uint32_t)AppTimer::getInstance().getMillis(), (uint32_t)AppTimer::getInstance().millisPassed(timestamp), (uint32_t)timeoutNext);
+        loggif("%lu timestamp, millispassed %lu, timeout %lu\n", (uint32_t) AppTimer::getInstance().getMillis(), (uint32_t) AppTimer::getInstance().millisPassed(timestamp), (uint32_t) timeoutNext);
         timestamp = AppTimer::getInstance().getMillis();
         sendSms();
 
     }
+}
+
+void watchDogFeed() {
+    wdt_reset();
 }
 
 void setup() {
@@ -86,12 +91,15 @@ void setup() {
     Modem::getInstance().setPower(1);
 
     AppTimer::getInstance().registerCallback("keepAliveMessage", 60000L, keepAliveMessage);
+    AppTimer::getInstance().registerCallback("watchDogFeed", 1000L, watchDogFeed);
     AppTimer::getInstance().registerCallback("ledBlink", 500, ledBlink);
     AppTimer::getInstance().registerCallback("Console::receiveSerial", 10, Console::receiveSerial);
     AppTimer::getInstance().registerCallback("Modem::receiveSerial", 10, Modem::staticReceiveSerial);
     AppTimer::getInstance().registerCallback("Modem::stateMachineTimeoutHandler", 500, Modem::staticStateMachineTimeoutHandler);
     AppTimer::getInstance().registerCallback("testTimer", 1000, testTimer);
     AppTimer::getInstance().registerCallback("Vaga::staticTimerCallback", 10000, Vaga::staticTimerCallback, 10000);
+
+    wdt_enable(WDTO_8S);
     loggif("end\n");
 }
 
